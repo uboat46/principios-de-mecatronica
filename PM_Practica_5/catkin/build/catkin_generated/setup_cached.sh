0b13592ecbcd29ef9af6d225be9894375970e378
robotica <robotica@labcl08.inge.itam.mx>
2019-11-22 10:40:38 -0600

    add catkin space

---
+++ PM_Practica_5/catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/robotica/Desktop/principios-de-mecatronica/PM_Practica_5/catkin/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/melodic/lib:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins"
+export PWD="/home/robotica/Desktop/principios-de-mecatronica/PM_Practica_5/catkin/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/robotica/Desktop/principios-de-mecatronica/PM_Practica_5/catkin/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/robotica/Desktop/principios-de-mecatronica/PM_Practica_5/catkin/src:$ROS_PACKAGE_PATH"
